c175039a4b4c7f84142fb051dacf81b78c9435e9
Magnowys <106505777+Magnowys@users.noreply.github.com>
2023-08-30 23:11:59 -0300

    Add files via upload

--- src/Sounds/events.js
+++ src/Sounds/events.js
@@ -6,9 +6,22 @@ import state from "./state.js"
 export function florest() {
     controls.florest.addEventListener("click", () => {
         state.isMute = document.documentElement.classList.toggle("select")
-        controls.florest.classList.toggle("select")
         
-        state.isMute == true ? sounds.forest.play() : sounds.forest.pause()
+        if (state.isMute == true) {
+            controls.florest.classList.add("select")
+            sounds.forest.play()
+            sounds.rain.pause()
+            controls.rain.classList.remove("select")
+            sounds.coffe.pause()
+            controls.coffe.classList.remove("select")
+            sounds.fire.pause()
+            controls.fire.classList.remove("select")
+            return
+        } else {
+
+            controls.florest.classList.remove("select")
+            sounds.forest.pause()
+        }
         
     })
 }
@@ -16,9 +29,20 @@ export function florest() {
 export function rain() {
     controls.rain.addEventListener("click", () => {
         state.isMute = document.documentElement.classList.toggle("select")
-        controls.rain.classList.toggle("select")
         
-        state.isMute == true ? sounds.rain.play() : sounds.rain.pause()
+        if (state.isMute == true) {
+            controls.rain.classList.add("select")
+            sounds.rain.play()
+            sounds.forest.pause()
+            controls.florest.classList.remove("select")
+            sounds.coffe.pause()
+            controls.coffe.classList.remove("select")
+            sounds.fire.pause()
+            controls.fire.classList.remove("select")
+            return
+        }
+        controls.rain.classList.remove("select")
+        sounds.rain.pause()
         
     })
 }
@@ -26,19 +50,39 @@ export function rain() {
 export function coffe() {
     controls.coffe.addEventListener("click", () => {
         state.isMute = document.documentElement.classList.toggle("select")
-        controls.coffe.classList.toggle("select")
         
-        state.isMute == true ? sounds.coffe.play() : sounds.coffe.pause()
-        
+        if (state.isMute == true) {
+            controls.coffe.classList.add("select")
+            sounds.coffe.play()
+            sounds.forest.pause()
+            controls.florest.classList.remove("select")
+            sounds.rain.pause()
+            controls.rain.classList.remove("select")
+            sounds.fire.pause()
+            controls.fire.classList.remove("select")
+            return
+        }
+        controls.coffe.classList.remove("select")
+        sounds.coffe.pause()
     })
 }
 
 export function fire() {
     controls.fire.addEventListener("click", () => {
         state.isMute = document.documentElement.classList.toggle("select")
-        controls.fire.classList.toggle("select")
         
-        state.isMute == true ? sounds.fire.play() : sounds.fire.pause()
-        
+        if (state.isMute == true) {
+            controls.fire.classList.add("select")
+            sounds.fire.play()
+            sounds.forest.pause()
+            controls.florest.classList.remove("select")
+            sounds.rain.pause()
+            controls.rain.classList.remove("select")
+            sounds.coffe.pause()
+            controls.coffe.classList.remove("select")
+            return
+        }
+        controls.fire.classList.remove("select")
+        sounds.fire.pause()
     })
 }
